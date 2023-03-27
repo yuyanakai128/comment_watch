830afc398344eaa3aa1c5764bdeafb1954c92974
fandi ahmad <superdev195128@hotmail.com>
2023-03-27 18:40:24 +0900

    sadf

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <title>コメント監視システム</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+        <meta content="コメント監視システム" name="description" />
         <meta content="Coderthemes" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <!-- App favicon -->
