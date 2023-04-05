6f53ae8987699b91427cfb45d792bee2eb51e262
fandi ahmad <superdev195128@hotmail.com>
2023-04-05 14:15:52 +0900

    asdf

--- app/Console/Commands/SendNotification.php
+++ app/Console/Commands/SendNotification.php
@@ -133,6 +133,7 @@ class SendNotification extends Command
             $this->info('double comment');
         }else{
             $this->sendEmail($goods,$comment);
+            sleep(1);
         }
     }
 
